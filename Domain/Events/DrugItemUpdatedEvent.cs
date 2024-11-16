8b86e4540aefa6b06449e118c531456f05510c30
Anna Salkutsan <salkutsanann@gmail.com>
2024-11-16 15:56:44 +0200

    Domain Event (2)

---
+++ Domain/Events/DrugItemUpdatedEvent.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Interfaces;
+
+namespace Domain.Events;
+
+/// <summary>
+/// Доменное событие обновления единицы лекарства.
+/// </summary>
+public sealed class DrugItemUpdatedEvent : IDomainEvent
+{
+    public Guid DrugItemId { get; }
+    public double OldCount { get; }
+    public double NewCount { get; }
+
+    public DrugItemUpdatedEvent(Guid drugItemId, double oldCount, double newCount)
+    {
+        DrugItemId = drugItemId;
+        OldCount = oldCount;
+        NewCount = newCount;
+    }
+}
---
+++ DomainTests/DomainEventTests.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Entities;
+using Domain.Events;
+using FluentValidation;
+
+namespace DomainTests;
+
+public class DomainEventTests
+{
+    [Fact]
+    public void UpdateCount_ShouldAddDomainEvent()
+    {
+        // Arrange
+        var drugItem = new DrugItem(Guid.NewGuid(), Guid.NewGuid(), 100m, 10, null, null);
+    
+        // Act
+        drugItem.UpdateCount(15);
+
+        // Assert
+        var events = drugItem.GetDomainEvents();
+        Assert.Single(events);
+        var @event = Assert.IsType<DrugItemUpdatedEvent>(events.First());
+        Assert.Equal(10, @event.OldCount);
+        Assert.Equal(15, @event.NewCount);
+    }
+
+    [Fact]
+    public void UpdateCount_InvalidCount_ShouldThrowValidationException()
+    {
+        // Arrange
+        var drugItem = new DrugItem(Guid.NewGuid(), Guid.NewGuid(), 100m, 10, null, null);
+    
+        // Act & Assert
+        Assert.Throws<ValidationException>(() => drugItem.UpdateCount(-5));
+    }
+
+}
